9ec957eeda7f2db06b3aec7041a93b2cf5071359
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-01-01 10:17:50 -0800

    removed old folder

--- iso-creation/02_Cleanup_the_ChRoot_Environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source "`dirname \"$0\"`/configuration.sh"
-
-chroot "$chroot" "/iso-creation/02_Cleanup_the_ChRoot_Environment.sh"
-
-echo "02 > unmounting /dev"
-umount "$chroot/dev"
--- iso-creation/chroot-update/iso-creation/gpg-keys.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
-wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
-wget -q https://dl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add -
-sudo wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-wget -q http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_16.04/Release.key -O- | sudo apt-key add -
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 886DDD89
