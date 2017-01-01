8e11aaece854b5072f0e2aeaea314abb9e858d94
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-01-01 12:14:54 -0800

    added filesystem size

--- toiso/compress_the_file_system.sh
+++ toiso/compress_the_file_system.sh
@@ -7,3 +7,5 @@ source "configuration.sh"
 echo "command > compress the file system"
 
 mksquashfs / "$image/casper/filesystem.squashfs" -ef ./exclude.txt -wildcards 
+
+printf $(sudo du -sx --block-size=1 / | cut -f1) > "$image/casper/filesystem.size"
