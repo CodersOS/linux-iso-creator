35c291acb2552b3336231627c016b2df8216f72f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-01-02 07:56:29 -0800

    removing container after build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-sudo docker build . -t codersos/linux-iso-creator && docker run codersos/linux-iso-creator /toiso/command.sh > CodersOS.iso
+sudo docker build . -t codersos/linux-iso-creator && docker run --rm codersos/linux-iso-creator /toiso/command.sh > CodersOS.iso
 echo built codersos/linux-iso-creator
